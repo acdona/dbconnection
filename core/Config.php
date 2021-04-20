f8570f507a562d8af93d6838033ea5d4af97435e
Antonio Carlos <acdona@hotmail.com>
2021-04-20 11:17:11 -0300

    updating gitignore

---
+++ core/Config.php
@@ -0,0 +1,19 @@
+<?php
+
+        define('URL', 'http//localhost/dbconnection/');
+     
+        define('CONTROLLER', 'Login');
+        define('METHOD', 'index');
+        define('CONTROLLERERROR', 'Error');
+
+        define('EMAILADM', 'webmaster@yoursite.com');
+
+        //Database access credentials
+        define('DBTYPE', 'mysql');
+        define('HOST',   'localhost');
+        define('USER',   'root');
+        define('PASS',   '');
+        define('DBNAME', 'newsphp');
+        define('PORT',   3306);
+
+?>
